b2b89e57abe9a7e9e6356c856b1681174f3f3022
sofiaki <sokiriazi@gmail.com>
2022-09-22 00:46:51 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const apps = {
+  Android: 'https://play.google.com/',
+  iOS: 'https://www.apple.com/store',
+};
+
+const platform = () => {
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/windows phone/i.test(userAgent)) return 'Windows Phone';
+  if (/android/i.test(userAgent)) return 'Android';
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) return 'iOS';
+  return null;
+};
+
+const redirect = () => {
+  let os = platform();
+  location.replace('http://www.magelectronicslab.eu');
+  
+};
+
+redirect();
